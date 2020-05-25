646a9cdc3d3b092734dd4626561e23cc948358d0
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-05-25 18:01:34 +0200

    first commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,26 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Aneta Stojanowska | Frontend Web Developer', // e.g: 'Name | Developer'
+  lang: 'nl, en, pl', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello, my name is',
+  name: 'Aneta Stojanowska',
+  subtitle: `I'm a Frontend Web Developer`,
+  cta: 'Hire me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: `Energetic and enthusiastic Frontend Web Developer with knowledge of design, frontend and backend and experience with database technology and UX issues. Able to build a working and professional web application. Functions as a bridge between the various disciplines, monitors agile principles and ensures that both backend and frontend work smoothly towards the same goals.`,
+  paragraphTwo: `Strives to work in teams with other developers and find efficient solutions for customers with complex needs.  I’m an open source contributor and enjoy participating in hackathons.`,
+  paragraphThree: `In my free time I write blog posts for development community and help other developers understand a topic, or with their code.`,
+  resume: 'https://medium.com/@anetastojanowska', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,11 +29,11 @@ export const projectsData = [
   {
     id: uuidv1(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'React app Robofriends from the scratch.',
+    info: `Although lightweight, Robofriends application is making use of ReactJS library. This application generates a list of users from an API. The features include filtering users, search box, scroll bar, loading time and error boundary message and customized logo.`,
+    info2: 'Technologies used: React, RESTful API, Git, Visual Studio Code, NPM',
+    url: 'https://aneta-s.github.io/robofriends/',
+    repo: 'https://github.com/aneta-s/robofriends', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
@@ -57,9 +57,9 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: `Let’s do something amazing.`,
+  btn: 'Contact',
+  email: 'a.stojanowska23@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +68,27 @@ export const footerData = {
     {
       id: uuidv1(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/a_stojanowska',
     },
     {
       id: uuidv1(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/aneta-s',
     },
     {
       id: uuidv1(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/stojanowska',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/aneta-s',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
