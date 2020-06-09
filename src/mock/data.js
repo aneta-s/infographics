2a3742e4a5b21c17aed56cabeb0ca1bfec166b49
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-06-09 22:36:38 +0200

    images

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,16 +11,16 @@ export const headData = {
 export const heroData = {
   title: 'Hello, my name is',
   name: 'Aneta Stojanowska',
-  subtitle: `I'm a Frontend Web Developer`,
-  cta: 'Hire me',
+  subtitle: `I'm a Frontend Web Developer.`,
+  cta: 'Find more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: `Energetic and enthusiastic Frontend Web Developer with knowledge of design, frontend and backend and experience with database technology and UX issues. Able to build a working and professional web application. Functions as a bridge between the various disciplines, monitors agile principles and ensures that both backend and frontend work smoothly towards the same goals.`,
-  paragraphTwo: `Strives to work in teams with other developers and find efficient solutions for customers with complex needs.  I’m an open source contributor and enjoy participating in hackathons.`,
-  paragraphThree: `In my free time I write blog posts for development community and help other developers understand a topic, or with their code.`,
+  paragraphTwo: `I’m an open source contributor and enjoy participating in hackathons. In my spare time I write blog posts for the development community to help other developers understand a topic and their code.`,
+  paragraphThree: ``,
   resume: 'https://medium.com/@anetastojanowska', // if no resume, the button will not show up
 };
 
@@ -28,36 +28,36 @@ export const aboutData = {
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: 'React app Robofriends from the scratch.',
-    info: `Although lightweight, Robofriends application is making use of ReactJS library. This application generates a list of users from an API. The features include filtering users, search box, scroll bar, loading time and error boundary message and customized logo.`,
-    info2: 'Technologies used: React, RESTful API, Git, Visual Studio Code, NPM',
+    img: 'react.jpg',
+    title: 'React app Robofriends from scratch.',
+    info: `Although lightweight, the Robofriends application is making use of ReactJS library. This application generates a list of users from an API. The features include filtering users, search box, scroll bar, loading time, error boundary message and a customized logo.`,
+    info2: 'Technologies used: React.js, RESTful API, Git, Visual Studio Code, NPM.',
     url: 'https://aneta-s.github.io/robofriends/',
     repo: 'https://github.com/aneta-s/robofriends', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'generator.jpg',
+    title: 'Gradient generator in JavaScript.',
+    info: `This gradient generator is not a full website, but rather an online tool that generates gradient backgrounds in JavaScript. The user uses buttons to blend two colors for a customized rainbow effect. It also generates a random backgrounds, using two random numbers for the color inputs.`,
+    info2: 'Technologies used: JavaScript, Bootstrap, HTML, CSS3.',
+    url: 'https://aneta-s.github.io/gradient/',
+    repo: 'https://github.com/aneta-s/gradient', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'resume.jpg',
+    title: 'Resume/CV website in Bootstrap 4.',
+    info: `A clean, simple and open source resume/CV landing page, featuring fixed side navigation with scrolling page anchors. Resume sections contain work experience, education, skills and stylish icons.`,
+    info2: 'Technologies used: JavaScript, Bootstrap 4, SCSS, HTML.',
+    url: 'https://aneta-s.github.io/startbootstrap-resume/',
+    repo: 'https://github.com/aneta-s/startbootstrap-resume', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: `Let’s do something amazing.`,
+  cta: `Strives to work in teams with other developers and find efficient solutions for customers with complex needs. Let’s do something amazing.`,
   btn: 'Contact',
   email: 'a.stojanowska23@gmail.com',
 };
