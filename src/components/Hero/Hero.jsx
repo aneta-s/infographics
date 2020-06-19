6f1c547db5eb1bd1d9612dbdb223edf82522adf4
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-06-19 14:17:54 +0200

    seo description

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,15 +26,15 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Aneta Stojanowska'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || "I'm a Frontend Web Developer."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <a className="cta-btn cta-btn--hero" href="#about">
-              {cta || 'Know more'}
+              {cta || 'Contact'}
             </a>
           </p>
         </Fade>
