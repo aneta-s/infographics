358f45634e08fa363c832126b227d0e917324307
aneta-s <54153719+aneta-s@users.noreply.github.com>
2021-03-15 18:32:06 +0100

    my first commit

---
+++ PROJECTEN/GATSBY-UXPROCESS/UWV/README.md
@@ -0,0 +1,3 @@
+License 📄
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ PROJECTEN/GATSBY-UXPROCESS/UWV/src/components/Contact/Contact.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container } from 'react-bootstrap';
+import PortfolioContext from '../../context/context';
+import Title from '../Title/Title';
+
+const Contact = () => {
+  const { contact } = useContext(PortfolioContext);
+  const { cta, btn, url, repo } = contact;
+
+  return (
+    <section id="contact">
+      <Container>
+        <Title title="" />
+        <Fade bottom duration={1000} delay={800} distance="30px">
+          <div className="contact-wrapper">
+            <p className="contact-wrapper__text" />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cta-btn cta-btn--resume"
+              href={url || '#!'}
+            >
+              {btn || ''}
+            </a>
+            {repo && (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cta-btn cta-btn--resume"
+                href={url || '#!'}
+              >
+                {cta || ''}
+              </a>
+            )}
+          </div>
+        </Fade>
+      </Container>
+    </section>
+  );
+};
+
+export default Contact;
